7535db5c895e60337d7691c6c421c8f654322806
ckscks7878jj <twodc@naver.com>
2025-08-29 15:15:30 +0900

    레벨 2-3: Validation

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,8 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,9 @@ public class SignupRequest {
     @NotBlank @Email
     private String email;
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp = ".*\\d.*")
+    @Pattern(regexp = ".*[A-Z].*")
     private String password;
     @NotBlank
     private String userRole;
--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,9 +1,12 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 
 @Getter
 @NoArgsConstructor
@@ -12,6 +15,10 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp = ".*\\d.*")
+    @Pattern(regexp = ".*[A-Z].*")
     private String newPassword;
 }
